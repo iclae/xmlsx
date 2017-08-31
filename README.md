f9c2446b8c48266e9451863bdef3a6b61c18a3df
lix <iclae0@gmail.com>
2017-09-01 05:32:10 +0800

    add example

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # xmlsx
 xmlsx = xml + xlsx
+
+## example
+
+    var xmlsx = new XMLSX()
+
+    xmlsx
+     .frozen('2')
+     .write([
+       ['我', '秦始皇', '打钱'],
+       ['aa', 'bb']]
+      )
+     .valid([
+       { A1: [2, 4, 6] },
+       { B5: ['f1', 'f4', 'qb'] }
+      ])
+     .getXlsx(function(err, buf) {
+       fs.writeFile('test.xlsx', buf)
+      })
--- xmlsx.js
+++ xmlsx.js
@@ -48,15 +48,4 @@ XMLSX.prototype.valid = function(validArray) {
   return this
 }
 
-// example
-// var xmlsx = new XMLSX()
-
-// xmlsx
-//   .frozen('2')
-//   .write([['我', '秦始皇', '打钱'], ['aa', 'bb']])
-//   .valid([{ A1: [2, 4, 6] }, { B5: ['f1', 'f4', 'qb'] }])
-//   .getXlsx(function(err, buf) {
-//     fs.writeFile('test.xlsx', buf)
-//   })
-
 module.exports = XMLSX
