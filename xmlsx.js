5edc9d54c887519451e420a52549fa24100a078d
Lix <iclae0@gmail.com>
2017-09-12 15:22:41 +0800

    0.2.5
    add format date cell tool func
    node>=4.0

--- xmlsx.js
+++ xmlsx.js
@@ -6,52 +6,53 @@ var valid = require('./lib/mod/valid')
 var hide = require('./lib/mod/hide')
 
 // main
-function XMLSX(buffer) {
+function Xmlsx(buffer) {
   this._store = new Store()
   this._store.cache(buffer)
 }
 
 // Objec func
-XMLSX.getCell = util.getCellByXY
+Xmlsx.getCell = util.getCellByXY
+Xmlsx.formatDate = util.formatDate
 
 // ------- step func
 // write data [[row],[row],[row]]
-XMLSX.prototype.entry = function(data) {
+Xmlsx.prototype.entry = function(data) {
   this._store.load(data)
   return this
 }
 
 // frozen row TODO: frozen col
-XMLSX.prototype.frozen = function(range) {
+Xmlsx.prototype.frozen = function(range) {
   frozen.setFrozen(this._store, range)
   return this
 }
 
 // valid data [{A1: [1, 2, 3]}, {'B1:B100': [a, b, c]}]
-XMLSX.prototype.valid = function(validArray) {
+Xmlsx.prototype.valid = function(validArray) {
   valid.setValid(this._store, validArray)
   return this
 }
 
 // hide cell 'A1'
-XMLSX.prototype.hide = function(cell) {
+Xmlsx.prototype.hide = function(cell) {
   hide.setHide(this._store, cell)
   return this
 }
 
 // ------- final func
 // get xlsx buffer
-XMLSX.prototype.done = function(callback) {
+Xmlsx.prototype.done = function(callback) {
   this._store.compile()
   this._store.build(callback)
 }
 
 // get xlsx object
-XMLSX.prototype.output = function() {
+Xmlsx.prototype.output = function() {
   if (this._store.opens.frozen) frozen.parseFrozen(this._store)
   if (this._store.opens.valid) valid.parseValid(this._store)
   this._store.parse()
   return this._store.clear()
 }
 
-module.exports = XMLSX
+module.exports = Xmlsx
